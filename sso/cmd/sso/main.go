18a1ec90bd798e415b2abe3d211b675e885d9c9f
Almir Avhadiev <almir.avhadiev@gmail.com>
2025-07-16 16:58:28 +0300

    Update .gitignire. Save for merge

--- sso/cmd/sso/main.go
+++ sso/cmd/sso/main.go
@@ -1,12 +1,17 @@
 package main
 
 import (
+	"fmt"
 	"log/slog"
 	"os"
 	"os/signal"
+	"syscall"
+
 	"sso/config"
 	"sso/internal/app"
-	"syscall"
+
+	"github.com/joho/godotenv"
+	"gopkg.in/yaml.v3"
 )
 
 const (
@@ -16,30 +21,38 @@ const (
 )
 
 func main() {
-	// TODO: инициализировать объект конфига
-	config := config.MustLoad()
+	// Load .env variables
+	_ = godotenv.Load("/app/.env")
 
-	// TODO: инициализировать логгер
-	log := initLogger(config.Env)
+	// Read and expand config.yaml
+	raw, err := os.ReadFile("/app/config/config.yaml")
+	if err != nil {
+		panic(fmt.Errorf("could not read config.yaml: %w", err))
+	}
+	expanded := os.ExpandEnv(string(raw))
+
+	var cfg config.Config
+	if err := yaml.Unmarshal([]byte(expanded), &cfg); err != nil {
+		panic(fmt.Errorf("could not unmarshal config.yaml: %w", err))
+	}
+
+	// Initialize logger
+	log := initLogger(cfg.Env)
 	log.Info("Initializing logger")
 	log.Debug("Initializing debug mode")
 
-	// TODO: инициализировать приложение (app)
-	application := app.New(log, config.GRPC.Port, config.ConnectionString, config.TokenTTL)
+	// Initialize app
+	application := app.New(log, cfg.GRPC.Port, cfg.ConnectionString, cfg.TokenTTL)
 
-	// TODO: запустить gRPC-сервер приложения
 	go func() {
 		application.GRPCServer.MustRun()
 	}()
 
-	//TODO: Завершить работу программы
 	stop := make(chan os.Signal, 1)
 	signal.Notify(stop, syscall.SIGTERM, syscall.SIGINT)
-	
 	<-stop
 
-	application.GRPCServer.Stop() 
-	//TODO: Add close for db
+	application.GRPCServer.Stop()
 	log.Info("Gracefully stopped")
 }
 
@@ -47,18 +60,13 @@ func initLogger(env string) *slog.Logger {
 	var log *slog.Logger
 	switch env {
 	case envLocal:
-		log = slog.New(
-			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
-		)
+		log = slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
 	case envDev:
-		log = slog.New(
-			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
-		)
+		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
 	case envProd:
-		log = slog.New(
-			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
-		)
+		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}))
+	default:
+		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}))
 	}
-
 	return log
 }
